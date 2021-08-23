353035cd0f58d78c7d7fe169c11886cd15e70a12
Agustin Alba Chicar <ag.albachicar@ekumenlabs.com>
2021-08-23 10:14:28 -0300

    Replaces kDut by dut and completes docstring.

--- maliput_py/test/api/api_test.py
+++ maliput_py/test/api/api_test.py
@@ -32,63 +32,64 @@ class TestMaliputApi(unittest.TestCase):
         """
         Tests the RoadGeometryId binding.
         """
-        kDut = RoadGeometryId("dut")
-        self.assertEqual("dut", kDut.string())
+        dut = RoadGeometryId("dut")
+        self.assertEqual("dut", dut.string())
 
     def test_inertial_position(self):
         """
         Tests the InertialPosition binding.
         """
-        kDut = InertialPosition(x=1., y=2., z=3.)
-        self.assertEqual(Vector3(1., 2., 3.), kDut.xyz())
+        dut = InertialPosition(x=1., y=2., z=3.)
+        self.assertEqual(Vector3(1., 2., 3.), dut.xyz())
 
     def test_lane_position(self):
         """
         Tests the LanePosition binding.
         """
-        kDut = LanePosition(s=1., r=2., h=3.)
-        self.assertEqual(Vector3(1., 2., 3.), kDut.srh())
+        dut = LanePosition(s=1., r=2., h=3.)
+        self.assertEqual(Vector3(1., 2., 3.), dut.srh())
 
     def test_lane_position_result(self):
         """
         Tests the LanePositionResult binding.
         """
-        kDut = LanePositionResult(lane_position=LanePosition(1., 2., 3.),
-                                  nearest_position=InertialPosition(4., 5., 6.),
-                                  distance=7.)
-        self.assertEqual(Vector3(1., 2., 3.), kDut.lane_position.srh())
-        self.assertEqual(Vector3(4., 5., 6.), kDut.nearest_position.xyz())
-        self.assertEqual(7., kDut.distance)
+        dut = LanePositionResult(lane_position=LanePosition(1., 2., 3.),
+                                 nearest_position=InertialPosition(4., 5., 6.),
+                                 distance=7.)
+        self.assertEqual(Vector3(1., 2., 3.), dut.lane_position.srh())
+        self.assertEqual(Vector3(4., 5., 6.), dut.nearest_position.xyz())
+        self.assertEqual(7., dut.distance)
 
     def test_empty_lane_position_result(self):
         """
         Tests an empty LanePositionResult binding.
         """
-        kDut = LanePositionResult()
-        self.assertEqual(Vector3(0., 0., 0.), kDut.lane_position.srh())
-        self.assertEqual(Vector3(0., 0., 0.), kDut.nearest_position.xyz())
-        self.assertEqual(0., kDut.distance)
+        dut = LanePositionResult()
+        self.assertEqual(Vector3(0., 0., 0.), dut.lane_position.srh())
+        self.assertEqual(Vector3(0., 0., 0.), dut.nearest_position.xyz())
+        self.assertEqual(0., dut.distance)
 
     def test_empty_road_position(self):
         """
         Tests an empty RoadPosition binding.
         """
-        kDut = RoadPosition()
-        self.assertEqual(None, kDut.lane)
-        self.assertEqual(Vector3(0., 0., 0.), kDut.pos.srh())
+        dut = RoadPosition()
+        self.assertEqual(None, dut.lane)
+        self.assertEqual(Vector3(0., 0., 0.), dut.pos.srh())
 
     def test_identity_rotation(self):
         """
-        Tests an Rotation binding.
+        Tests an empty Rotation binding.
         """
-        kDut = Rotation()
-        self.assertEqual(Vector4(1., 0., 0., 0.), kDut.quat().coeffs())
-        self.assertAlmostEqual(0., kDut.rpy().roll_angle())
-        self.assertAlmostEqual(0., kDut.rpy().pitch_angle())
-        self.assertAlmostEqual(0., kDut.rpy().yaw_angle())
+        dut = Rotation()
+        self.assertEqual(Vector4(1., 0., 0., 0.), dut.quat().coeffs())
+        self.assertAlmostEqual(0., dut.rpy().roll_angle())
+        self.assertAlmostEqual(0., dut.rpy().pitch_angle())
+        self.assertAlmostEqual(0., dut.rpy().yaw_angle())
 
     def test_lane_id(self):
         """
+        Tests the LaneId binding.
         """
-        kDut = LaneId("dut")
-        self.assertEqual("dut", kDut.string())
+        dut = LaneId("dut")
+        self.assertEqual("dut", dut.string())
